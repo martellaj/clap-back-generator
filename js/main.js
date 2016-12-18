23426c5b9b17ec523161698523cb91ff7c3541ed
Joe Martella <martellaj@live.com>
2016-12-18 13:51:48 -0800

    Initial commit.

---
+++ js/main.js
@@ -0,0 +1,41 @@
+// Get HTML elements that are necessary to clap back.
+const input = document.querySelector('#input');
+const hiddenOutput = document.querySelector('#hiddenOutput');
+const output = document.querySelector('#output');
+const message = document.querySelector('#message');
+
+input.addEventListener('change', clapBack);
+
+function clapBack() {
+    if (!this.value.trim()) {
+        output.textContent = '';
+        return;
+    }
+
+    const clap = '👏 ';
+
+    // Show clapped text.
+    let tokenizedInput = this.value.split(' ');
+    const clappedInput = tokenizedInput.join(clap);
+    output.textContent = clappedInput + clap;
+
+    // Copy to clipboard.
+    hiddenOutput.value = clappedInput + clap;
+    hiddenOutput.select();
+    const successful = document.execCommand('copy');
+
+    // Hide, clear, show.
+    hiddenOutput.style.display = 'none';
+    hiddenOutput.value = '';
+    hiddenOutput.style.display = 'block';
+
+    // Set focus to visible input.
+    input.focus();
+
+    // Show result message.
+    if (successful) {
+        message.textContent = 'Copied to clipboard. Go clap back.';
+    } else {
+        message.textContent = 'Unable to copy to clipboard. Sorry!';
+    }
+}
